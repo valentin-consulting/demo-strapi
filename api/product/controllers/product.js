7aec2d160de30960bae38b6c2c239524d8f53d9c
Valentin <valentin@valentin.consulting>
2021-01-18 17:12:48 +0400

    [feature] Adding reverse_name controller and endpoint

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -1,8 +1,20 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  reverse_name: async (ctx) => {
+    const { id } = ctx.params;
+    const entity = await strapi.services.product.findOne({ id });
+    const sanitized = sanitizeEntity(entity, {
+      model: strapi.models.product,
+    });
+    if (typeof sanitized.name === "string")
+      return sanitized.name.split("").reverse().join("");
+    else throw new Error("Data in not available");
+  },
+};
